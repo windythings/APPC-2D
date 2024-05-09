c5d8291b3498ce924ebbf070bb096fa4bb8515e0
Himavath <hjois2@illinois.edu>
2024-05-09 15:04:56 -0500

    Better memory pre-allocation for faster running

--- calcAirfoilMatrices.m
+++ calcAirfoilMatrices.m
@@ -9,56 +9,27 @@ function [A,B] = calcAirfoilMatrices(surfaces)
 %          B - the influence coefficient matrix used to calculate the
 %          induced velocity of a panel at another location in the domain
 
-influence = struct('name', cell(2,2));
-totalMatrixSize = 0;
+totalMatrixSize = sum([surfaces.n]);
+colOffset = 1;
+rowOffset = 1;
 
-% For loop calculates the influence matrices for each airfoil surface's
-% influence on itself and the other(s). Stores all of those influence
-% coefficient matrices in the "influence" data structure.
-for p = 1:length(surfaces)
-    totalMatrixSize = totalMatrixSize + surfaces(p).n;
-    
-    for q = 1:length(surfaces)
-        influence(p,q).name = "On " + num2str(p) + " Due To " + num2str(q);
-        [influence(p,q).A,influence(p,q).B] = ...
-                                  calcVelMatricesFast(surfaces(p),surfaces(q));
-    end
-end
+A = zeros(totalMatrixSize,totalMatrixSize);
+B = zeros(totalMatrixSize,totalMatrixSize);
 
-Atemp = [];
-Btemp = [];
-A = [];
-B = [];
-
-% Enforcing Kutta Condition for the appropriate influence matrices (those
-% of a surface's influence on itself).
 for p = 1:length(surfaces)
     for q = 1:length(surfaces)
-        
-        influence(p,q).A(end+1,:) = zeros(1,length(influence(p,q).A(end,:)));
-        influence(p,q).B(end+1,:) = zeros(1,length(influence(p,q).B(end,:)));
-        
-        % Enforcing Kutta Condition at trailing edge elements
-        if p==q
-            influence(p,q).A(end,1) = 1;
-            influence(p,q).A(end,end) = 1;
+        [A(rowOffset:rowOffset+surfaces(p).m-1,colOffset:colOffset+surfaces(q).m),...
+         B(rowOffset:rowOffset+surfaces(p).m-1,colOffset:colOffset+surfaces(q).m)] = ...
+                            calcVelMatricesFast(surfaces(p),surfaces(q));
+        if p == q
+            A(rowOffset+surfaces(p).m,colOffset) = 1;            
+            A(rowOffset+surfaces(p).m,colOffset+surfaces(q).m) = 1;
         end
+        
+        colOffset = colOffset + surfaces(q).n;
     end
-end
-
-% Concatenates all of the influence matrices into a giant A matrix and B
-% matrix, plus an RHS vector. This contains all of the influence
-% coefficients and can be inverted to find the airfoil gammas.
-for p = 1:length(surfaces)
-    for q = 1:length(surfaces)
-        Atemp = [Atemp influence(p,q).A];
-        Btemp = [Btemp influence(p,q).B];
-    end
-    
-    A = [A; Atemp];
-    B = [B; Btemp];
-    Atemp = [];
-    Btemp = [];
+    colOffset = 1;
+    rowOffset = rowOffset + surfaces(p).n;
 end
 
 end
