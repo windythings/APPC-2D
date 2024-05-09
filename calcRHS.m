fcd30638df312fc5b6af6789a90fa4f928b600bf
Himavath <hjois2@illinois.edu>
2024-05-09 16:37:55 -0500

    Better memory pre-allocation for faster running

--- calcRHS.m
+++ calcRHS.m
@@ -7,76 +7,40 @@ function [RHS] = calcRHS(surfaces,wake)
 % Outputs: RHS - augmented RHS vector accounting for freestream and wake 
 %                induced velocities on the airfoil
 
-RHS = [];
 
-% The normalized freestream component normal to a panel is just sin(theta),
-% where theta is the panel angle relative to the freestream. This step is
-% standard for most panel codes.
+RHS = zeros(sum([surfaces.n]),1);
+rowOffset = 0;
+
 for p = 1:length(surfaces)
-    for i = 1:length(surfaces(p).theta)
-         RHStemp(i,1) = sin(surfaces(p).theta(i));
-    end
-    RHStemp(end+1,1) = 0; % enforce zero Kutta condition for trailing edge elements
+    indexList = (1:surfaces(p).m)+rowOffset;
     
-    RHS = [RHS; RHStemp];
-    RHStemp = [];
-end
-
-% Calculate the induction on the airfoil surfaces due to the farfield
-% circulation shed downstream of the system and sum this with the
-% freestream induction component.
-vInfNorm_airfoil_farField = [];
-
-for j = 1:length(wake)-1
-    for i = 1:length(surfaces)
-        [tempNorm,~] = calcFarFieldInduction(surfaces(i),wake(j));
-        tempNorm(end+1,1) = 0; % enforce zero Kutta condition for trailing edge elements
-        vInfNorm_airfoil_farField = [vInfNorm_airfoil_farField; tempNorm];
-    end
+    % The normalized freestream component normal to a panel is just 
+    % sin(theta), where theta is the panel angle relative to the 
+    % freestream. This step is standard for most panel codes.
+    RHS(indexList) = RHS(indexList) + sin(surfaces(p).theta);
     
-    RHS = RHS + vInfNorm_airfoil_farField;
-    vInfNorm_airfoil_farField = [];
-end
-
-% Calculate the influence of the wake boundaries themselves on the RHS of
-% the airfoil surface circulation system. Per definition, these are
-% subtracted from RHS as they are defined relative to the normal vector
-% inside the airfoil surfaces.
-temp = [];
-vInfNorm_airfoil_wake = [];
-
-for j = 1:length(wake)-1
-    for i = 1:length(surfaces)
-        % Notation: Matrix Influence_on Airfoil Surface_due to Wake Surface
-        [A_airfoil_wake,~] = calcVelMatricesFast(surfaces(i),wake(j));
-        temp = A_airfoil_wake*wake(j).gamma;
+    for q = 1:length(wake)-1
+        % Calculate the induction on the airfoil surfaces due to the 
+        % farfield circulation shed downstream of the system and sum this 
+        % with the freestream induction component.
+        RHS(indexList) = RHS(indexList) + calcFarFieldInduction(surfaces(p),wake(q));
+        
+        % Calculate the influence of the wake boundaries themselves on the 
+        % RHS of the airfoil surface circulation system. Per definition, 
+        % these are subtracted from RHS as they are defined relative to the
+        % normal vector inside the airfoil surfaces.
+        [A_airfoil_wake,~] = calcVelMatricesFast(surfaces(p),wake(q));
+        RHS(indexList) = RHS(indexList) - A_airfoil_wake*wake(q).gamma;
         
         % Augment the Kutta Condition at the trailing edge of the airfoil
         % surfaces such that the airfoil circulation matches the wake
         % circulation, ensuring smooth circulation distributions.
-        if j == 1
-            if i == 1
-%                 temp(end+1,1) = -wake(j).gamma(1);
-                temp(end+1,1) = 0;
-            else
-                temp(end+1,1) = 0; 
-            end
-        elseif j == 2
-            if i == 2
-%                 temp(end+1,1) = -wake(j).gamma(1);
-                temp(end+1,1) = 0;
-            else
-                temp(end+1,1) = 0;
-            end
-        else
-            temp(end+1,1) = 0;
+        if p == q
+            RHS(indexList(end)+1) = wake(q).gamma(1);
         end
-        vInfNorm_airfoil_wake = [vInfNorm_airfoil_wake; temp];
     end
     
-    RHS = RHS - vInfNorm_airfoil_wake;
-    temp = [];
-    vInfNorm_airfoil_wake = [];
+    rowOffset = rowOffset + surfaces(p).n;
 end
 
 end
