9342a3baa54db860625bd3102bcf8131a0db76f3
unknown <alansh2@illinois.edu>
2024-07-10 04:03:40 -0500

    package-free unstructured contour plot

--- pltC.m
+++ pltC.m
@@ -9,119 +9,83 @@ function graf = pltC(surfaces,foil,wake,opts)
 
     if strcmpi(opts.plot,'contour')
         % Plots the velocity magnitude field on an efficient grid
+        filename = mfilename('fullpath');
+        filepath = fileparts( filename );
+        addpath([filepath,'/mesh2d']);
+
+        initmsh();
+
+        % Define area of interest
+        node = [xmax+ch/2, ymax+ch/4;
+                xmin-ch/2, ymax+ch/4;
+                xmin-ch/2, ymin-ch/4;
+                xmax+ch/2, ymin-ch/4];
+
+        % Place evaluation points barely outside the boundary because
+        % exact boundary evaluation is unstable
         bubble = repmat({[]},1,nSurf); % the neighborhood of points near the solid surfaces
-        product_info = ver('pde'); % check if the Matlab PDE toolbox is available
-        if isempty(product_info)
-            % Create a structured mesh using the package-free method
-            SW = [xmin ymin];
-            NE = [xmax ymax];
-            m0 = 0;
-            for i = 1:nSurf
-                bubble{i} = offsetBoundary(surfaces{i},foil.theta((1:foil.m(i))+m0),0.0125*ch);
-                % The resulting boundary can be self-intersecting
-                % Generate a concave hull to guarantee a simple polygon
-                bubble{i} = ashape(bubble{i},0.1);    % The alpha shape routine is faster than
-                % bubble{i} = concavehull(bubble{i}); % k-neighbors concave hull but needs a
-                SW = min(SW,min(bubble{i},[],1));     % user specified alpha radius that can
-                NE = max(NE,max(bubble{i},[],1));     % differ between geometries
-                m0 = m0 + foil.m(i);
-            end
-
-            % FINE mesh
-            [X,Y] = meshgrid(linspace(SW(1),NE(1),301),linspace(SW(2),NE(2),301));
-            inchk = false(size(X));
-            for i = 1:nSurf
-                in = inpolygon(X,Y,bubble{i}(:,1),bubble{i}(:,2));
-                inchk(in) = true; % retain points that are inside the bubble
-            end
-            for i = 1:nSurf
-                in = inpolygon(X(inchk),Y(inchk),surfaces{i}(:,1),surfaces{i}(:,2));
-                inchk(inchk) = ~in; % omit points that are inside the solid surfaces
-            end
-            finex = X(inchk);
-            finey = Y(inchk);
-
-            % COARSE mesh
-            [X,Y] = meshgrid(linspace(xmin-ch/2,xmax+ch/2,76),linspace(ymin-ch/4,ymax+ch/4,76));
-            inchk = true(size(X));
-            for i = 1:nSurf
-                in = inpolygon(X,Y,bubble{i}(:,1),bubble{i}(:,2));
-                inchk(in) = false;
-            end
-            coarx = X(inchk);
-            coary = Y(inchk);
-
-            % WAKE mesh
-            wx = [linspace(wake.xo(1)+1e-4,xmax+ch/2,200) linspace(wake.xo(N+1)+1e-4,xmax+ch/2,200)].';
-            wy = [interp1(wake.xo(1:N),wake.yo(1:N),wx(1:200))-1e-8; interp1(wake.xo(N+1:2*N),wake.yo(N+1:2*N),wx(201:400))+1e-8];
-            wt = [interp1(wake.xo(1:N),wake.theta(1:N),wx(1:200)); interp1(wake.xo(N+1:2*N),wake.theta(N+1:2*N),wx(201:400))];
-            i = wy > ymin-ch/4-1e-4;
-            ep = (-1:1)*(NE(2)-SW(2))/300;
-            wx = reshape(wx(i) + ep.*cos(wt(i) + pi/2),[],1);
-            wy = reshape(wy(i) + ep.*sin(wt(i) + pi/2),[],1);
-
-            % TOTAL mesh
-            field.xc = [finex; coarx; wx];
-            field.yc = [finey; coary; wy];
-        else
-            % Create unstructured mesh using Matlab PDE Toolbox
-            % Place evaluation points barely outside the boundary because
-            % exact boundary evaluation is unstable
-            m0 = 0;
-            for i = 1:nSurf
-                bubble{i} = offsetBoundary(surfaces{i},foil.theta((1:foil.m(i))+m0),1e-6);
-                m0 = m0 + foil.m(i);
-            end
-
-            % Get only the wake points relevant to the view area
-            i = (wake.xo < xmax+ch/2) & (wake.yo > ymin-ch/4);
-
-            % Set lower wake evaluation points also offset from exact boundary
-            j = find(i(1:N));
-            j(end+1) = j(end) + 1; % add point that crosses outside domain
-            wkl = [wake.xo(j) wake.yo(j)];
-            un = mod(wake.theta(j)+pi/2,2*pi); % angles of wake panel normal vectors
-            un = [cos(un) sin(un)]; % convert angles to vectors
-            wkl = [wkl-5e-6*un; flipud(wkl+5e-6*un)];
-            wkl(1,:) = bubble{1}(1,:); % match the airfoil TE
-            wkl(end,:) = bubble{1}(end,:);
-
-            % Repeat for upper wake
-            j = find(i(N+1:2*N)) + N;
-            j(end+1) = j(end) + 1;
-            wku = [wake.xo(j) wake.yo(j)];
-            un = mod(wake.theta(j)+pi/2,2*pi);
-            un = [cos(un) sin(un)];
-            wku = [wku-5e-6*un; flipud(wku+5e-6*un)];
-            wku(1,:) = bubble{2}(1,:);
-            wku(end,:) = bubble{2}(end,:);
-
-            % Build geometry matrix
-            dim1 = max([foil.m+1 length(wkl) length(wku) 10]); % no. of rows containing point data
-            gm(:,nSurf+3) = [3 4 xmax+ch/2 xmin([1 1])-ch/2 xmax+ch/2, ...
-                          ymin([1 1])-ch/4 ymax([1 1])+ch/4 zeros(1,2*(dim1-4))].'; % domain box
-            gm(:,nSurf+2) = [2; length(wku); wku(:); zeros(2*(dim1-length(wku)),1)]; % upper wake
-            gm(:,nSurf+1) = [2; length(wkl); wkl(:); zeros(2*(dim1-length(wkl)),1)]; % lower wake
-            % Assign W1, W2, and E1 name-spaces to lower and upper wakes
-            % and box environment, respectively
-            ns(nSurf+(1:3),:) = char('W1','W2','E1');
-            m0 = 0;
-            for i = 1:nSurf
-                % Complete matrix with airfoils
-                gm(:,i) = [2; foil.m(i)+1; bubble{i}(:); zeros(2*(dim1-foil.m(i)-1),1)];
-                ns(i,:) = ['S' num2str(i)];
-                m0 = m0 + foil.m(i);
-            end
-            sf = ['E1-W1-W2' sprintf('-S%d',1:nSurf)]; % set formula subtracts airfoils from the domain
-            g = decsg(gm,sf,ns.');
-
-            % Create mesh using the toolbox functions
-            domain = createpde;
-            geometryFromEdges(domain,g);
-            msh = generateMesh(domain,'Hmin',1e-3); % Hmin may need to adapt to geometry
-            field.xc = msh.Nodes(1,:).';
-            field.yc = msh.Nodes(2,:).';
+        m0 = 0;
+        for i = 1:nSurf
+            bubble{i} = offsetBoundary(surfaces{i},foil.theta((1:foil.m(i))+m0),1e-4);
+            m0 = m0 + foil.m(i);
         end
+
+        % Add airfoils to the boundary nodes
+        node = cat(1,node,bubble{:});
+
+        % Create adjacency matrix to track edges
+        M = false(size(node,1));
+        M(1:4,1:4) = periodicAdjMatrix(4);
+        len = 4;
+        for i = 1:nSurf
+            r = size(bubble{i},1);
+            M(len+(1:r),len+(1:r)) = periodicAdjMatrix(r);
+            len = len + r;
+        end
+
+%        [node(:,1),k] = sort(node(:,1));
+%        node(:,2) = node(k,2);
+%        M = M(k,:);
+
+        index = repmat((1:len).',1,len);
+        edge = reshape(index(M),[2 len]).';
+
+%        [edge(:,1),k] = sort(edge(:,1));
+%        edge(:,2) = edge(k,2);
+
+        % Mesh generation
+        opts.kind = 'delfront';
+        opts.rho2 = 1;
+        [vert,etri,tria,tnum] = refine2(node,edge,[],opts);
+
+        % Insert wake
+        % Get only the wake points relevant to the view area
+        i = (wake.xo < xmax+ch/2) & (wake.yo > ymin-ch/4);
+
+        % Set lower wake evaluation points also offset from exact boundary
+        j = find(i(1:N));
+        j(end+1) = j(end) + 1; % add point that crosses outside domain
+        wkl = interp1(1:8:8*(length(j)-1)+1,[wake.xo(j) wake.yo(j)],1:8*(length(j)-1)+1);
+
+        % Repeat for upper wake
+        j = find(i(N+1:2*N)) + N;
+        j(end+1) = j(end) + 1;
+        wku = interp1(1:8:8*(length(j)-1)+1,[wake.xo(j) wake.yo(j)],1:8*(length(j)-1)+1);
+
+        wk = [bubble{1}(1,:) - wkl(1,:) + flipud(wkl);
+              bubble{1}(end,:) - wkl(1,:) + wkl;
+              bubble{2}(1,:) - wku(1,:) + flipud(wku);
+              bubble{2}(end,:) - wku(1,:) + wku];
+
+        % Remove instabilities in the shear layer
+        [in,on] = inpoly2(vert,wk(1:2*size(wkl,1),:),[1:2*size(wkl,1);2:2*size(wkl,1) 1].');
+        vert(in|on,:) = [];
+        [in,on] = inpoly2(vert,wk(2*size(wkl,1)+(1:2*size(wku,1)),:),[1:2*size(wku,1);2:2*size(wku,1) 1].');
+        vert(in|on,:) = [];
+
+        % Evaluate velocities at the mesh vertices
+        field.xc = [vert(:,1); wk(:,1)];
+        field.yc = [vert(:,2); wk(:,2)];
         field.dx = ones(size(field.xc));    % Grid points are the control points
         field.dy = zeros(size(field.xc));   % of the influence calculation.
         field.xo = field.xc - 0.5;          % They are placed at the center of
@@ -139,10 +103,10 @@ function graf = pltC(surfaces,foil,wake,opts)
         % Make contour plot
         DT = delaunay(field.xc,field.yc);
         patch(field.xc(DT).',field.yc(DT).',sqrt(u(DT).^2+v(DT).^2).','LineStyle','none')
+        if strcmpi(opts.mesh,'on'), triplot(DT,field.xc,field.yc,'k-'); end
         for i = 1:nSurf
             fill(surfaces{i}(:,1),surfaces{i}(:,2),[1 1 1])
         end
-        if strcmpi(opts.mesh,'on'), triplot(DT,field.xc,field.yc); end
         daspect([1 1 1])
         cm = Colormap;
         colormap(cm.(lower(opts.colormap))(400))
@@ -216,4 +180,11 @@ function newBoundary = offsetBoundary(inputBoundary,angles,offset)
     movedir(1,:) = [cos(tnormal(1)) sin(tnormal(1))];
     movedir(end,:) = [cos(tnormal(end)) sin(tnormal(end))];
     newBoundary = inputBoundary + offset*movedir; % expand points outward
+end
+
+function M = periodicAdjMatrix(n)
+    % PERIODICADJMATRIX helper function for generating the adjacency
+    % matrix for a n-segment periodic curve.
+    M = logical(eye(n) + diag(ones(1,n-1),1));
+    M(n,1) = true;
 end
